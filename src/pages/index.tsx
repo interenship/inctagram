5a7794fdf6af317883c4c040ee627fd58198ad31
yahve0110 <ilik124darc@gmail.com>
2024-09-04 23:04:57 +0300

    IN-5 fix: remove unnecessary changes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,5 @@
 import Image from "next/image";
 import { Inter } from "next/font/google";
-import {
-  Select,
-  SelectContent,
-  SelectItem,
-  SelectTrigger,
-  SelectValue,
-} from "@/shared/ui/Select";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -14,17 +7,6 @@ export default function Home() {
   return (
     <div>
       <h1>Inctagram</h1>
-      <Select>
-        <SelectTrigger className="w-[180px]">
-          <SelectValue placeholder="Select-box" />
-        </SelectTrigger>
-        <SelectContent>
-          <SelectItem value="Select-box">Select-box</SelectItem>
-          <SelectItem value="dark">Dark</SelectItem>
-          <SelectItem value="system">System</SelectItem>
-          <SelectItem value="def">Def</SelectItem>
-        </SelectContent>
-      </Select>
     </div>
   );
 }

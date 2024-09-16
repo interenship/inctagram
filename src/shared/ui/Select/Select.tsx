bdc3f6071dbfd41db742f81cf096c976163dc21a
yahve0110 <ilik124darc@gmail.com>
2024-09-16 11:50:48 +0300

    selectRefactor refactor: refactor styles and fix full width of select

--- src/shared/ui/Select/Select.tsx
+++ src/shared/ui/Select/Select.tsx
@@ -9,30 +9,11 @@ import {
   Viewport,
   Value,
 } from "@radix-ui/react-select";
-import { cn } from "@/features/utils/cn";
 import ChevronDown from "@/shared/assets/icons/components/chevron-down";
+import { cn } from "@/features/utils/cn";
 
 const Select = Root;
 const SelectValue = Value;
-
-const styles = {
-  trigger: cn(
-    "group flex text-light-100 h-9 w-full items-center leading-6 justify-between rounded-sm border border-dark-100 bg-background px-3 py-1.5 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:border-blue-500 focus:ring-0 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1",
-  ),
-  content: cn(
-    "relative z-50 max-h-96 w-[180px] overflow-hidden rounded-sm border bg-popover text-popover-foreground shadow-md",
-    "data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0",
-    "data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95",
-    "data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-  ),
-  contentPopper: cn(
-    "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
-  ),
-  viewport: cn("p-1"),
-  viewportPopper: cn(
-    "h-[var(--radix-Select-trigger-height)] w-full min-w-[var(--radix-Select-trigger-width)]",
-  ),
-};
 
 type SelectTriggerProps = React.ComponentPropsWithoutRef<typeof Trigger> & {
   IconComponent?: React.ComponentType<React.SVGProps<SVGSVGElement>>;
@@ -45,7 +26,11 @@ const SelectTrigger = React.forwardRef<
   <Trigger
     ref={ref}
     disabled={disabled}
-    className={cn(styles.trigger, className)}
+    className={cn(
+      "group flex text-light-100 h-9 w-full rounded-[2px] items-center leading-6 justify-between border border-dark-100 bg-background px-3 py-1.5 text-sm ring-offset-background placeholder:text-muted-foreground focus:outline-none focus:border-blue-500 focus:ring-0 disabled:cursor-not-allowed disabled:opacity-50 [&>span]:line-clamp-1 active:border-light-100",
+      "data-[state=open]:border-white",
+      className,
+    )}
     {...props}
   >
     {IconComponent && <IconComponent className="mr-2" />}
@@ -66,8 +51,9 @@ const SelectContent = React.forwardRef<
     <Content
       ref={ref}
       className={cn(
-        styles.content,
-        position === "popper" && styles.contentPopper,
+        "relative z-50 max-h-96 overflow-hidden border rounded-[2px] border-light-100 bg-dark-900 text-popover-foreground shadow-md data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
+        position === "popper" &&
+          "data-[side=bottom]:translate-y-1 data-[side=left]:-translate-x-1 data-[side=right]:translate-x-1 data-[side=top]:-translate-y-1",
         className,
       )}
       position={position}
@@ -75,8 +61,8 @@ const SelectContent = React.forwardRef<
     >
       <Viewport
         className={cn(
-          styles.viewport,
-          position === "popper" && styles.viewportPopper,
+          position === "popper" &&
+            "h-[var(--radix-select-trigger-height)] w-full min-w-[var(--radix-select-trigger-width)]",
         )}
       >
         {children}

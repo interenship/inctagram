396c6e2f059a4ae9b84ca4a8df82bed4fac01cf4
yahve0110 <ilik124darc@gmail.com>
2024-09-27 13:57:57 +0300

    IN-21 refactor: sidebar refactor

--- src/shared/ui/Sidebar/Sidebar.tsx
+++ src/shared/ui/Sidebar/Sidebar.tsx
@@ -1,118 +1,23 @@
 import React from "react";
-import {
-  Bookmark,
-  BookmarkOutline,
-  Home,
-  HomeOutline,
-  LogOutOutline,
-  MessageCircle,
-  MessageCircleOutline,
-  Person,
-  PersonOutline,
-  PlusSquare,
-  PlusSquareOutline,
-  SearchOutline,
-  TrendingUp,
-  TrendingUpOutline,
-} from "@/shared/assets/icons/components";
-import { Typography } from "@/shared/ui/Typography";
-import { Button } from "@/shared/ui/Button";
 import { MenuItem } from "@/shared/ui/Sidebar/MenuItem";
-
-const menuItems = {
-  home: { href: "/", label: "Home", icon: HomeOutline, filledIcon: Home },
-  create: {
-    href: "/create",
-    label: "Create",
-    icon: PlusSquareOutline,
-    filledIcon: PlusSquare,
-  },
-  profile: {
-    href: "/profile",
-    label: "My profile",
-    icon: PersonOutline,
-    filledIcon: Person,
-  },
-  messenger: {
-    href: "/messenger",
-    label: "Messenger",
-    icon: MessageCircleOutline,
-    filledIcon: MessageCircle,
-  },
-  search: { href: "/search", label: "Search", icon: SearchOutline },
-  statistics: {
-    href: "/statistics",
-    label: "Statistics",
-    icon: TrendingUpOutline,
-    filledIcon: TrendingUp,
-  },
-  favorites: {
-    href: "/favorites",
-    label: "Favorites",
-    icon: BookmarkOutline,
-    filledIcon: Bookmark,
-  },
-};
-
-const logOut = () => {};
+import { menuItemsData } from "@/shared/ui/Sidebar/menuItemsData";
+import LogOutBtn from "@/shared/ui/Sidebar/LogOutBtn/LogOutBtn";
+const { home, create, profile, messenger, search, statistics, favorites } =
+  menuItemsData;
 
 export const Sidebar = () => {
+  const logOut = () => {};
+
   return (
-    <nav className="w-[220px] h-screen bg-dark-700 text-light-100 pl-[40px] flex flex-col justify-center items-start border-r-[1px] border-dark-300">
-      <MenuItem
-        href={menuItems.home.href}
-        label={menuItems.home.label}
-        icon={menuItems.home.icon}
-        filledIcon={menuItems.home.filledIcon}
-        className="mt-[72px]"
-      />
-      <MenuItem
-        href={menuItems.create.href}
-        label={menuItems.create.label}
-        icon={menuItems.create.icon}
-        filledIcon={menuItems.create.filledIcon}
-      />
-      <MenuItem
-        href={menuItems.profile.href}
-        label={menuItems.profile.label}
-        icon={menuItems.profile.icon}
-        filledIcon={menuItems.profile.filledIcon}
-      />
-      <MenuItem
-        href={menuItems.messenger.href}
-        label={menuItems.messenger.label}
-        icon={menuItems.messenger.icon}
-        filledIcon={menuItems.messenger.filledIcon}
-      />
-      <MenuItem
-        href={menuItems.search.href}
-        label={menuItems.search.label}
-        icon={menuItems.search.icon}
-        className="mb-[60px]"
-      />
-      <MenuItem
-        href={menuItems.statistics.href}
-        label={menuItems.statistics.label}
-        icon={menuItems.statistics.icon}
-        filledIcon={menuItems.statistics.filledIcon}
-      />
-      <MenuItem
-        href={menuItems.favorites.href}
-        label={menuItems.favorites.label}
-        icon={menuItems.favorites.icon}
-        filledIcon={menuItems.favorites.filledIcon}
-        className="mb-[140px]"
-      />
-      <Button
-        variant="text"
-        className="text-light-100 cursor-pointer flex gap-3 group mb-[180px] focus:ring-2 focus:ring-accent-500 focus:outline-none rounded"
-        onClick={logOut}
-      >
-        <LogOutOutline className="text-light-100 group-hover:text-accent-100 group-active:text-accent-500" />
-        <Typography.MEDIUM14 className="group-hover:text-accent-100 group-active:text-accent-500">
-          Log out
-        </Typography.MEDIUM14>
-      </Button>
+    <nav className="w-[220px] h-screen bg-dark-700 text-light-100 pl-[40px] flex flex-col justify-center items-start border-r-[1px] border-dark-300 pt-[72px]">
+      <MenuItem {...home} />
+      <MenuItem {...create} />
+      <MenuItem {...profile} />
+      <MenuItem {...messenger} />
+      <MenuItem {...search} className="mb-[60px]" />
+      <MenuItem {...statistics} />
+      <MenuItem {...favorites} className="mb-[140px]" />
+      <LogOutBtn logOutFn={logOut} />
     </nav>
   );
 };
---
+++ src/shared/ui/Sidebar/menuItemsData.ts
@@ -0,0 +1,50 @@
+import {
+  Bookmark,
+  BookmarkOutline,
+  Home,
+  HomeOutline,
+  MessageCircle,
+  MessageCircleOutline,
+  Person,
+  PersonOutline,
+  PlusSquare,
+  PlusSquareOutline,
+  SearchOutline,
+  TrendingUp,
+  TrendingUpOutline,
+} from "@/shared/assets/icons/components";
+
+export const menuItemsData = {
+  home: { href: "/", label: "Home", icon: HomeOutline, filledIcon: Home },
+  create: {
+    href: "/create",
+    label: "Create",
+    icon: PlusSquareOutline,
+    filledIcon: PlusSquare,
+  },
+  profile: {
+    href: "/profile",
+    label: "My profile",
+    icon: PersonOutline,
+    filledIcon: Person,
+  },
+  messenger: {
+    href: "/messenger",
+    label: "Messenger",
+    icon: MessageCircleOutline,
+    filledIcon: MessageCircle,
+  },
+  search: { href: "/search", label: "Search", icon: SearchOutline },
+  statistics: {
+    href: "/statistics",
+    label: "Statistics",
+    icon: TrendingUpOutline,
+    filledIcon: TrendingUp,
+  },
+  favorites: {
+    href: "/favorites",
+    label: "Favorites",
+    icon: BookmarkOutline,
+    filledIcon: Bookmark,
+  },
+};

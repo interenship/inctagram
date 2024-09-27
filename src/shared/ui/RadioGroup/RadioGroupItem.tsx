60ad29222350c642f97aa7e9a13ca08eb41f467f
Timofeev Yury <yurik-007@mail.ru>
2024-09-27 09:45:49 +0300

    IN-04 refactor: add id.

--- src/shared/ui/RadioGroup/RadioGroupItem.tsx
+++ src/shared/ui/RadioGroup/RadioGroupItem.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
+import { ComponentPropsWithoutRef, ElementRef, forwardRef, useId } from "react";
 import { Indicator, Item } from "@radix-ui/react-radio-group";
 import { cn } from "@/features/utils/cn";
 
@@ -6,18 +6,22 @@ type RadioGroupItemProps = ComponentPropsWithoutRef<typeof Item>;
 
 const RadioGroupItem = forwardRef<ElementRef<typeof Item>, RadioGroupItemProps>(
   (props, ref) => {
-    const { className, disabled = false, ...restProps } = props;
+    const { className, disabled = false, id, ...restProps } = props;
+
+    const generatedId = useId();
+    const finalId = id ?? generatedId;
 
     return (
       <Item
         ref={ref}
         className={cn(
-          "group flex justify-center items-center relative rounded-full border-2 border-light-100 w-5 h-5 transition-all duration-300  ",
+          "group flex justify-center items-center relative rounded-full border-2 border-light-100 w-5 h-5 transition-all duration-300",
           disabled
             ? "cursor-not-allowed opacity-50"
             : "hover:bg-dark-300 hover:shadow-[0px_0px_0px_6px_#333333] focus:bg-dark-500 focus:shadow-[0px_0px_0px_6px_#171717] active:bg-dark-100 active:shadow-[0px_0px_0px_6px_#4c4c4c]",
           className,
         )}
+        id={finalId}
         {...restProps}
       >
         <Indicator

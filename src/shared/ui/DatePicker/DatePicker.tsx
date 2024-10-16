d02a65a5081cb2282cc6930f34c810c17e8d1a18
Timofeev Yury <yurik-007@mail.ru>
2024-10-17 01:11:39 +0300

    create component

---
+++ src/shared/ui/DatePicker/DatePicker.tsx
@@ -0,0 +1,201 @@
+import { format, isValid, parse } from "date-fns";
+import type { ComponentPropsWithoutRef } from "react";
+import React, { forwardRef, useId, useState } from "react";
+import type { DateRange } from "react-day-picker";
+import { DayPicker, getDefaultClassNames } from "react-day-picker";
+
+import { cn } from "@/features/utils/cn";
+import SvgCalendar from "@/shared/assets/icons/components/Calendar";
+import SvgCalendarOutline from "@/shared/assets/icons/components/CalendarOutline";
+import { Label } from "@/shared/ui/Label";
+import { Typography } from "@/shared/ui/Typography";
+
+type DatePickerProps = ComponentPropsWithoutRef<"input"> & {
+  disabled?: boolean;
+  errorMessage?: string;
+  onClick?: () => void;
+  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+};
+
+export const DatePicker = forwardRef<HTMLInputElement, DatePickerProps>((props, ref) => {
+  const {
+    disabled = false,
+    errorMessage,
+    // onClick,
+    // onChange,
+    className,
+    // value,
+    ...restProps
+  } = props;
+
+  const inputId = useId();
+
+  const [month, setMonth] = useState<Date>(new Date());
+
+  const [selectedRange, setSelectedRange] = useState<DateRange | undefined>(undefined);
+
+  const [inputValue, setInputValue] = useState("");
+  const [showCalendar, setShowCalendar] = useState<boolean>(false);
+  const [error, setError] = useState<undefined | string>(errorMessage);
+
+  const handleDayPickerSelect = (range: DateRange | undefined) => {
+    if (!range || !range.from) {
+      setInputValue("");
+      setError("Please select a valid date range.");
+      return;
+    }
+
+    const fromDate = format(range.from, "dd/MM/yyyy");
+
+    if (!range.to || range.from.getTime() === range.to.getTime()) {
+      setInputValue(fromDate);
+      setError(undefined);
+    } else {
+      const toDate = format(range.to, "dd/MM/yyyy");
+      setInputValue(`${fromDate} - ${toDate}`);
+      setError(undefined);
+    }
+    setSelectedRange(range);
+  };
+  const toggleCalendarVisibility = () => {
+    setShowCalendar(prev => !prev);
+  };
+  const handleInputClick = (e: React.MouseEvent<HTMLInputElement>) => {
+    e.currentTarget.blur(); // Убираем фокус с input после клика
+    toggleCalendarVisibility();
+  };
+  const handleIconClick = () => {
+    toggleCalendarVisibility();
+  };
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { value } = e.target;
+
+    const regex = /^(0[1-9]|[12][0-9]|3[01])\/(0[1-9]|1[0-2])\/(19|20)\d\d$/;
+
+    if (regex.test(value) || value === "") {
+      setInputValue(value);
+      const parsedDate = parse(value, "dd/MM/yyyy", new Date());
+
+      if (isValid(parsedDate)) {
+        setSelectedRange({ from: parsedDate, to: undefined });
+        setMonth(parsedDate);
+        setError(undefined);
+      } else {
+        setSelectedRange(undefined);
+        setError("Invalid date.");
+      }
+    } else {
+      setError("Date must be in DD/MM/YYYY format.");
+    }
+  };
+  const isFirstDay = (day: Date) => {
+    return selectedRange?.from ? day.getTime() === selectedRange.from.getTime() : false;
+  };
+  const isLastDay = (day: Date) => {
+    return selectedRange?.to ? day.getTime() === selectedRange.to.getTime() : false;
+  };
+  const isMiddleDay = (day: Date) => {
+    if (!selectedRange?.from || !selectedRange?.to) return false;
+    return day > selectedRange.from && day < selectedRange.to;
+  };
+
+  const defaultClassNames = getDefaultClassNames();
+  console.log(defaultClassNames);
+  return (
+    <div className="w-[310px] text-light-100">
+      <Label htmlFor={inputId} disabled={disabled}>
+        <Typography.REGULAR14 className="text-light-900">Date select:</Typography.REGULAR14>
+      </Label>
+      <div className={cn("group relative w-full", disabled && "pointer-events-none")}>
+        <input
+          className={cn(
+            "h-9 w-full rounded-sm border border-dark-300 bg-transparent px-2.5 py-1.5 text-base font-normal text-light-100",
+            "enabled:active:border-dark-300 enabled:active:bg-dark-500 enabled:active:outline-none",
+            "enabled:hover:border-dark-100 enabled:hover:bg-dark-500 enabled:hover:outline-none",
+            "enabled:focus:border-accent-700 enabled:focus:bg-dark-500 enabled:focus:outline-none",
+            // "pointer-events-none cursor-default",
+            disabled ? "cursor-not-allowed opacity-50" : "hover:cursor-pointer",
+            error && "border-danger-500 bg-dark-500 text-danger-500",
+            className
+          )}
+          id={inputId}
+          type="text"
+          value={inputValue}
+          placeholder="DD/MM/YYYY"
+          onClick={handleInputClick}
+          onChange={handleInputChange}
+          disabled={disabled}
+          ref={ref}
+          {...restProps}
+        />
+
+        {!showCalendar && (
+          <SvgCalendarOutline
+            onClick={handleIconClick}
+            className={cn(
+              "absolute right-1 top-1.5 cursor-pointer fill-light-100",
+              // disabled && "fill-dark-100",
+              error && "fill-danger-500"
+            )}
+          />
+        )}
+
+        {showCalendar && (
+          <SvgCalendar
+            onClick={handleIconClick}
+            className={cn("absolute right-1 top-1.5 cursor-pointer fill-light-100")}
+          />
+        )}
+      </div>
+      {showCalendar && (
+        <DayPicker
+          classNames={{
+            root: cn(
+              `${defaultClassNames.root}`, // Основные стили ${defaultClassNames.root}
+              "rounded-sm border border-dark-300 bg-dark-500 p-6 font-normal" // Дополнительные стили для корневого элемента
+            ),
+            day: "w-[36px] h-[36px] text-center  hover:bg-accent-700 hover:rounded-full active:rounded-full active:bg-accent-900 focus:ring-2 focus:ring-accent-300",
+            today: "font-bold text-accent-500 ",
+            month_caption: cn(
+              // `${defaultClassNames.month_caption}`,
+              "flex items-center justify-between" // Используйте flex для выравнивания
+            ),
+            caption_label: " text-light-100 font-bold py-1.5 pl-2 leading-6 pb-5",
+            chevron: cn(
+              "size-[20px] rounded-full bg-dark-100 fill-light-100 shadow-[0_0_0px_6px_rgba(76,76,76,1)] hover:bg-dark-300 hover:shadow-[0_0_0px_6px_rgba(51,51,51,1)]"
+            ),
+            nav: "relative",
+            button_previous: cn("absolute right-12 top-2"),
+            button_next: cn("absolute right-4 top-2"),
+            weekdays: cn("font-normal text-light-900"),
+          }}
+          modifiersClassNames={{
+            // Стили для субботы и воскресенья
+            weekEnd: "text-danger-300 no-hover no-focus no-active",
+            first: "rounded-l-full text-light-100 bg-accent-900", // Полукруглая левая сторона
+            last: "rounded-r-full text-light-100 bg-accent-900", // Полукруглая правая сторона
+            middle: "bg-accent-900  text-light-100",
+            outside: "text-light-900",
+          }}
+          weekStartsOn={1} //
+          showOutsideDays
+          month={month}
+          onMonthChange={setMonth}
+          mode="range"
+          selected={selectedRange}
+          onSelect={handleDayPickerSelect}
+          modifiers={{
+            weekEnd: { dayOfWeek: [0, 6] }, // Воскресенье Суббота
+            first: isFirstDay,
+            last: isLastDay,
+            middle: isMiddleDay,
+          }}
+        />
+      )}
+      Selected:{inputValue}
+      {error && <Typography.SMALL className={cn("text-danger-500")}>{error}</Typography.SMALL>}
+    </div>
+  );
+});
+DatePicker.displayName = "DatePicker";

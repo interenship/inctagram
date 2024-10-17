e98b44f316d8ae0cb371fa84eaca1588754b09db
eleonora <nebrudko18@gmail.com>
2024-10-17 09:07:45 +0300

    diff

--- src/shared/assets/icons/components/ArrowIosBack.tsx
+++ src/shared/assets/icons/components/ArrowIosBack.tsx
@@ -1,5 +1,6 @@
 import type { Ref, SVGProps } from "react";
 import { forwardRef, memo } from "react";
+
 const SvgArrowIosBack = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
--- src/shared/assets/icons/components/PlusSquareOutline.tsx
+++ src/shared/assets/icons/components/PlusSquareOutline.tsx
@@ -1,5 +1,5 @@
-import type { SVGProps } from "react";
-import { Ref, forwardRef, memo } from "react";
+import type { Ref, SVGProps } from "react";
+import { forwardRef, memo } from "react";
 
 const SvgPlusSquareOutline = (props: SVGProps<SVGSVGElement>, ref: Ref<SVGSVGElement>) => {
   const { className, ...restProps } = props;

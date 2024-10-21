7ea964a606284f373ce60a7938fdaff328767e6f
JK <viletert@gmail.com>
2024-10-21 15:01:57 +0300

    type prop added

--- src/shared/ui/Scrollbar/Scrollbar.tsx
+++ src/shared/ui/Scrollbar/Scrollbar.tsx
@@ -1,15 +1,17 @@
+import type * as ScrollArea from "@radix-ui/react-scroll-area";
 import { Root, Scrollbar, Thumb, Viewport } from "@radix-ui/react-scroll-area";
 import type { ComponentPropsWithoutRef } from "react";
 import React from "react";
 
 export type ScrollbarProps = {
   children: React.ReactNode;
+  type?: ScrollArea.ScrollAreaProps["type"];
 } & ComponentPropsWithoutRef<"div">;
 
 const ScrollBar = (props: ScrollbarProps) => {
-  const { children, ...restProps } = props;
+  const { children, type = "auto", ...restProps } = props;
   return (
-    <Root className="size-full overflow-hidden bg-inherit">
+    <Root className="size-full overflow-hidden bg-inherit" type={type}>
       <Viewport className="size-full">{children}</Viewport>
       <Scrollbar className="w-[4px] cursor-pointer" orientation="vertical">
         <Thumb className="flex-1 rounded-sm bg-dark-300 hover:bg-light-900" />
